53fd87a5cb0227dae9dbf1af14dcaf55c3f1000d
Asier Cidon <asier.cidon@gmail.com>
2020-12-09 11:38:51 +0100

    Added index path and multi jumps

--- jump.py
+++ jump.py
@@ -8,6 +8,13 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def home():
+    print("Received GET /")
+    res = Response("/ - Greetings from Python!", 200)
+    print(res.to_string())
+    return res.to_json()
+
 @app.route('/jump', methods=['GET', 'POST'])
 def jump():
     if request.method == 'GET':
@@ -21,17 +28,27 @@ def jump():
         print("Received POST /jump with 1 JUMP.jumps - " + str_data)
     
         jump = json.loads(str_data, object_hook=lambda d: Jump(**d))
-        res = Response("/ - Farewell from Python! Bad request by default", 400)
+        res = Response("/jump - Farewell from Python! Bad request by default", 400)
 
         if len(jump.jumps) == 1:
-            r = requests.get(jump.jumps[0] + jump.last_path)
+            url = jump.jumps[0] + jump.last_path
+            r = requests.get(url)
             r_data = json.dumps(r.json())
             res = json.loads(r_data, object_hook=lambda d: Response(**d))
 
         if len(jump.jumps) == 2:
-            res = Response("/ - Greetings from Python! 2 ", 200)
+            new_jump = jump
+            new_jump.jumps = new_jump.jumps[1:]
+            url = jump.jumps[0] + jump.jump_path
+            headers = {'Content-Type': 'application/json; utf-8', 'Accept': 'application/json'}
+            try:
+                p = requests.post(url , headers=headers, data=new_jump.to_json())
+                p_data = json.dumps(p.json())
+                res = json.loads(p_data, object_hook=lambda d: Response(**d))
+            except: 
+                res = Response("/jump - Farewell from Python! Error Jumping", 400)
 
         return res.to_json()
 
-    glob_res = Response("/ - Farewell from Python! - Bad Request", 400)
+    glob_res = Response("/jump - Farewell from Python! - Bad Request", 400)
     return glob_res.to_json()
