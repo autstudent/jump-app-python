35f9dbadbf540d038900b408825463cf931c9cb3
Asier Cidon <asier.cidon@gmail.com>
2021-03-10 20:02:01 +0100

    Added react-modifier header propagation

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def home():
     print("Received GET /")
+    headers = request.headers
+    print(headers)
+    
     res = Response("/ - Greetings from Python!", 200)
     print(res.to_string())
     return res.to_json()
@@ -19,6 +22,9 @@ def home():
 def jump():
     if request.method == 'GET':
         print("Received GET /jump")
+        headers = request.headers
+        print(headers)
+
         res = Response("/jump - Greetings from Python!", 200)
         print(res.to_string())
         return res.to_json()
@@ -26,13 +32,16 @@ def jump():
     if request.method == 'POST':
         str_data = json.dumps(request.get_json())
         print("Received POST /jump with 1 JUMP.jumps - " + str_data)
+        headers = request.headers
+        print(headers)
     
         jump = json.loads(str_data, object_hook=lambda d: Jump(**d))
         res = Response("/jump - Farewell from Python! Bad request by default", 400)
 
         if len(jump.jumps) == 1:
             url = jump.jumps[0] + jump.last_path
-            r = requests.get(url)
+            headers = {'Content-Type': 'application/json; utf-8', 'Accept': 'application/json', 'React-Modifier': headers['React-Modifier']}
+            r = requests.get(url , headers=headers)
             r_data = json.dumps(r.json())
             res = json.loads(r_data, object_hook=lambda d: Response(**d))
 
@@ -40,7 +49,7 @@ def jump():
             new_jump = jump
             new_jump.jumps = new_jump.jumps[1:]
             url = jump.jumps[0] + jump.jump_path
-            headers = {'Content-Type': 'application/json; utf-8', 'Accept': 'application/json'}
+            headers = {'Content-Type': 'application/json; utf-8', 'Accept': 'application/json', 'React-Modifier': headers['React-Modifier']}
             try:
                 p = requests.post(url , headers=headers, data=new_jump.to_json())
                 p_data = json.dumps(p.json())
